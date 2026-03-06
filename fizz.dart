86d1da308e4771ee806c36fe0b27be9d0035cce6
Anıl Murat Hırça <anilmurathirca@gmail.com>
2026-03-06 18:43:33 +0300

    finish fizzbuzz and start learn class in dart

---
+++ fizz.dart
@@ -0,0 +1,17 @@
+void main() {
+  int num = 1;
+
+  //loop
+  while (num <= 100) {
+    if (num % 3 == 0 && num % 5 == 0) {
+      print("FizzBuzz");
+    } else if (num % 3 == 0) {
+      print("Fizz");
+    } else if (num % 5 == 0) {
+      print("Buzz");
+    } else {
+      print(num);
+    }
+    num++;
+  }
+}
