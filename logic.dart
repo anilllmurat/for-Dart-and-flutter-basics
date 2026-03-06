407a602b9b6aed42ee2b27ba8408aca70d1ff340
Anıl Murat Hırça <anilmurathirca@gmail.com>
2026-03-06 18:00:45 +0300

    start learning dart functions

--- logic.dart
+++ logic.dart
@@ -1,1 +1,11 @@
-void main() {}
+void main() {
+  //logic   if, if else, if else if
+  var num = -7;
+  if (num > 0) {
+    print("Positive");
+  } else if (num < 0) {
+    print("Negative");
+  } else {
+    print("Zero");
+  }
+}
